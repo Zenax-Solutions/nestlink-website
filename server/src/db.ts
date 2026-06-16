0c1fc08bdebe144f10e49701e09486146f479c10
Supun Nethsara <52463797+Snrox@users.noreply.github.com>
2026-06-16 17:05:18 +0530

    Update db.ts

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,7 +1,10 @@
 import mysql from 'mysql2/promise'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import dotenv from 'dotenv'
 
-dotenv.config()
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
